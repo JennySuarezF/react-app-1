b1c7b813f353ab54779c43e3a72082e9fea201e4
JennySuarezF <150633206+JennySuarezF@users.noreply.github.com>
2023-11-22 17:29:43 +0000

    Añadidos adicionales

---
+++ src/components/Headers.jsx
@@ -0,0 +1,31 @@
+const Headers = () => {
+  return (
+    <div className="container-fluid text-center mt-5">
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="https://cdn0.ecologiaverde.com/es/posts/2/6/4/tipos_de_flores_mas_resistentes_2462_1_600.webp"
+            alt="Flor" width="140" height="140" className="rounded-circle" />
+          <h2 class="fw-normal">Heading</h2>
+          <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+          <p><button class="btn btn-secondary">View details &raquo;</button></p>
+        </div>
+        <div class="col-lg-4">
+          <img src="https://cdn0.ecologiaverde.com/es/posts/2/6/4/rosas_2462_2_600.webp"
+            alt="Flor" width="140" height="140" className="rounded-circle" />
+          <h2 class="fw-normal">Heading</h2>
+          <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+          <p><button class="btn btn-secondary">View details &raquo;</button></p>
+        </div>
+        <div class="col-lg-4">
+          <img src="https://cdn0.ecologiaverde.com/es/posts/2/6/4/tulipanes_2462_3_600.webp"
+            alt="Flor" width="140" height="140" className="rounded-circle" />
+          <h2 class="fw-normal">Heading</h2>
+          <p>And lastly this, the third column of representative placeholder content.</p>
+          <p><button class="btn btn-secondary">View details &raquo;</button></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Headers
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Carousel from "../components/Carousel"
+import Headers from "../components/Headers"
 
 const Home = () => {
   return (
     <>
       <Carousel />
+      <Headers />
     </>
   )
 }
