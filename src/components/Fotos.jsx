459d5654d3b60d2e3cfc03c7e83c90c820524eb9
JennySuarezF <150633206+JennySuarezF@users.noreply.github.com>
2023-11-22 16:40:24 +0000

    correcciones menores

--- src/components/Fotos.jsx
+++ src/components/Fotos.jsx
@@ -1,12 +1,4 @@
-const Fotos = () => {
-  const fotos = [
-    "https://www.nationalgeographic.com.es/medio/2018/02/27/playa-de-isuntza-lekeitio__1280x720.jpg",
-    "https://www.nationalgeographic.com.es/medio/2023/05/31/monte-elbrus_57f6f3c2_230531122513_1280x625.jpg",
-    "https://www.nationalgeographic.com.es/medio/2023/05/31/cafayate-star-factory-cafayate-salta-argentina_00000000_230602134845_1200x675.jpg",
-    "https://www.nationalgeographic.com.es/medio/2023/05/31/the-eyes-of-the-universe-bulgaria_00000000_230602134928_1200x499.jpg",
-    "https://www.nationalgeographic.com.es/medio/2023/05/31/night-under-the-baobab-trees-baobab-avenue-madagascar_00000000_230602135955_1200x853.jpg"
-  ]
-
+const Fotos = ({ fotos }) => {
   return (
     <div className="container-fluid text-center">
       <h1>Galería de fotos</h1>
--- src/pages/Galería.jsx
+++ src/pages/Galería.jsx
@@ -1,9 +1,17 @@
 import Fotos from "../components/Fotos"
 
 const Galeria = () => {
+  const fotos = [
+    "https://www.nationalgeographic.com.es/medio/2018/02/27/playa-de-isuntza-lekeitio__1280x720.jpg",
+    "https://www.nationalgeographic.com.es/medio/2023/05/31/monte-elbrus_57f6f3c2_230531122513_1280x625.jpg",
+    "https://www.nationalgeographic.com.es/medio/2023/05/31/cafayate-star-factory-cafayate-salta-argentina_00000000_230602134845_1200x675.jpg",
+    "https://www.nationalgeographic.com.es/medio/2023/05/31/the-eyes-of-the-universe-bulgaria_00000000_230602134928_1200x499.jpg",
+    "https://www.nationalgeographic.com.es/medio/2023/05/31/night-under-the-baobab-trees-baobab-avenue-madagascar_00000000_230602135955_1200x853.jpg"
+  ]
+
   return (
     <>
-      <Fotos />
+      <Fotos fotos={fotos} />
     </>
   )
 }
